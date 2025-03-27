2ef1ec0fc6ff8e3903d98b8587a9009335e4bfd8
rismanobahar <rismanobahar@gmail.com>
2025-03-27 15:01:24 +0700

    03/27/2025

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -26,9 +26,16 @@ class ReviewController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Book $book)
     {
-        //
+        $data = $request->validate([
+            'review' => 'required|min:15',
+            'rating' => 'required|min:1|max:5|integer'
+        ]);
+
+        $book->reviews()->create($data);
+
+        return redirect()->route('books.show', $book);
     }
 
     /**
