04a6578160aad55313f3c0315edeb981bb9048a3
rismanobahar <rismanobahar@gmail.com>
2025-03-29 23:59:23 +0700

    03/29/2025

--- resources/views/components/star-rating.blade.php
+++ resources/views/components/star-rating.blade.php
@@ -1,6 +1,6 @@
 @if ($rating)
     @for ($i = 1; $i <= 5; $i++)
-    {{ $i <= round($rating) ? '★' : '☆' }}
+    {{ $i <= round($rating) ? '★' : '☆' }} <!-- This will show the star rating based on the rating value. -->
     @endfor
 @else
 
